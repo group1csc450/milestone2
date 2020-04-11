b6413afafe656cbf3e6c7999988c9ffae45fcb53
Devin <devinanicholson@gmail.com>
2020-04-11 10:28:52 -0400

    Added connection code to Firestore for all categories

--- app/src/main/java/com/example/ratnabarot/recipeapp/Breakfast.java
+++ app/src/main/java/com/example/ratnabarot/recipeapp/Breakfast.java
@@ -3,15 +3,33 @@ package com.example.ratnabarot.recipeapp;
 import android.content.Intent;
 import android.os.Bundle;
 
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Breakfast extends AppCompatActivity {
+
+    RecyclerView recipe;
+    FirebaseFirestore fStore;
+
+    private FirestoreRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +38,85 @@ public class Breakfast extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fStore = FirebaseFirestore.getInstance();
+
+        recipe = findViewById(R.id.breakfast);
+
+        // create a reference to the recipe collection
+        CollectionReference recipesRef = fStore.collection("recipe");
+
+        // Create a query against the collection
+        Query query = recipesRef.whereEqualTo("categoryName", "Breakfast");
+
+        //Query
+        // Query query = fStore.collection("breakfast");
+
+        //RecyclerOptions
+        FirestoreRecyclerOptions<CategoryModel> options = new FirestoreRecyclerOptions.Builder<CategoryModel>()
+                .setQuery(query, CategoryModel.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<CategoryModel, CategoryViewHolder>(options) {
+
+
+
+            @NonNull
             @Override
-            public void onClick(View view) {
-                Intent login = new Intent (Breakfast.this, MainActivity.class);
-                startActivity(login);
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_single, parent, false);
+                return new CategoryViewHolder(view);
             }
-        });
+
+            @Override
+            protected void onBindViewHolder(@NonNull CategoryViewHolder holder, int position, @NonNull CategoryModel model) {
+
+                holder.list_name.setText(model.getRecipeName());
+                holder.list_desc.setText(model.getRecipeDescription());
+
+
+            }
+        };
+
+
+
+        recipe.setHasFixedSize(true);
+        recipe.setLayoutManager(new LinearLayoutManager(this));
+        recipe.setAdapter(adapter);
+
+
+    }
+
+    private class CategoryViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView list_name;
+        private TextView list_desc;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            list_name = itemView.findViewById(R.id.list_recipeName);
+            list_desc = itemView.findViewById(R.id.list_recipeDescription);
+
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (adapter != null) {
+            adapter.stopListening();
+
+        }
+
+
     }
 
     //Onclick event for smoothie
